f65c3ffd89f430cd54cf0c5aca712705eeb19168
Jose Matamoros <jose.andres.mm.04@gmail.com>
2018-11-24 13:13:28 -0600

    Ultimos comentarios a pi.c

--- Tarea3/Ejercicio_3/pi.c
+++ Tarea3/Ejercicio_3/pi.c
@@ -6,27 +6,27 @@
 
 float total=0;
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
-
+//Se define la estructura que va a tener los limites superioes e inferiores
 typedef struct arg_struct {
     int inferior;
     int superior;
 }suma_parcial;
-/* this function is run by the second thread */
-void *partial_calc(void *args){
-/* increment x to 100 */
+
+void *partial_calc(void *args){ // Se define la funcion que recibe cada thread
+
   suma_parcial *sum = (suma_parcial *)args;
   // printf("Limite inferior: %d\n", sum-> inferior);
   // printf("Limite superior: %d\n", sum-> superior);
-  float resultado_parcial =0;
+  float resultado_parcial =0; //Se define la variable que guarda el resultado final
 
-
+  // Se realiza la sumatoria parcial basado en los limites
   for (int i = (sum->inferior); i <=(sum->superior); i++) {
 
     resultado_parcial=resultado_parcial+4*powf(-1,(float)(i-1))/(2.0*((float)(i-1))+1.0);
     // printf("Resultado parcial %d\n",sum->resultado);
   }
   printf("Resultado Parcial: %.*f\n",10,resultado_parcial);
-  pthread_mutex_lock( &mutex1 );
+  pthread_mutex_lock( &mutex1 ); // Se usa mutex lock para que se pueda agregar el resultado a la variable compartida
   total=total+resultado_parcial;
   //printf("Resultado Parcial: %f\n",total);
   pthread_mutex_unlock( &mutex1 );
@@ -35,13 +35,14 @@ void *partial_calc(void *args){
 
 
 int main(int argc, char *argv[]){
-  clock_t start5=clock();
+  clock_t start5=clock(); // Se inicializa el cronometro del programa
   int thread_count = atoi(argv[2]);
 
   // printf("%d\n",thread_count);
   // pthread_t *threads = (new)[];
   // pthread_t threads[100];
   // suma_parcial sum_parc[100];
+  //Se crean los arreglos de los threads y de las estructuras con los limites
   pthread_t *threads = (pthread_t*)malloc(thread_count*sizeof(pthread_t*));
   suma_parcial *sum_parc = (suma_parcial*)malloc(thread_count*sizeof(suma_parcial*));
 //Se incializan los structs con los limites superiores, inferiores y los resultados parciales.
@@ -54,7 +55,7 @@ int main(int argc, char *argv[]){
   }
 
   printf("-----fin de inicializacion de de limites de sumatorios---------------------------\n");
-
+// Se crean todos los threads recorriendo el arreglo
   for (int i = 0; i < thread_count; i++) { // Se inicializan los threads pasando los structs con los limites.
     if(pthread_create(&threads[i], NULL, partial_calc, &sum_parc[i])) {
 
@@ -63,7 +64,7 @@ int main(int argc, char *argv[]){
 
     }
   }
-
+// Se unen todos los threads
   for (int i = 0; i < thread_count; i++) {
     if(pthread_join(threads[i], NULL)) {
 
@@ -74,7 +75,7 @@ int main(int argc, char *argv[]){
   }
   printf("------------------------------------------------------------------\n");
 
-
+//Se imprime el resultado final de la variable compoartida
   printf("Resultado total: %.*f\n",10,total);
   printf("Tiempo de ejecución: %.*f s\n",10,((double)clock()-start5)/CLOCKS_PER_SEC);
 
